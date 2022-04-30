5280bf59616b34e94a210c7fa1d6df454b00d27d
Giorgi <giosos2000@yahoo.com>
2022-04-30 11:16:16 +0200

    Test commit

---
+++ Java/HW1/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Please fix me");
+  }
+}
+// 1. Missed semicolon ( ; ) emd of thr line 3
+// 2. Missed closing curly bracket ( } )
---
+++ Java/HW1/Style.java
@@ -0,0 +1,5 @@
+public  class   Style {
+  static public void main(String[] args) {
+      System.out.println("Please make this class readable");
+  }
+}
